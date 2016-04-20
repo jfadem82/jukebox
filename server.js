b59329dfbbb0dd99e875df1711aa4e87a13ecc1b
Josh <jfadem82@gmail.com>
2016-04-20 15:14:24 -0700

    finishes basics of server.js

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ var expres 			= require('express'),
 	path			= require('path'),
 	mongoose		= require('mongoose'),
 	cors			= require('cors'),
+	apiRouter		= require('./api/routes/Routes'),
 	port 			= process.env.PORT || 3000
 	mongoUri		= process.env.mLab || 'mongodb://localhost:27017/jukebox'
 
 mongoose.connect(mongoUri)
 
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api', apiRouter)
+
+app.listen(port)
+console.log('listening on port ' + port)
