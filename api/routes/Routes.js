74b2cda8856bab9679f1247bc3634f2a18d6c1ad
Josh <jfadem82@gmail.com>
2016-04-25 04:06:16 -0700

    home state displays on 3000 when nodemon spins up edits to  passport, usersController, routes, and middleware in server.js authentication still not working

--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -1,13 +1,18 @@
 var express = require('express')
-var apiRouter = express.Router() //get an instance of express router
+var apiRouter = express.Router() //get an instance of express 
+var passport = require('passport')
+var bodyParser = require('body-parser')
 var usersController = require('../controllers/usersController')
 var User = require('../models/User')
 
 apiRouter.route('/users')
 	.post(usersController.create)
 
-apiRouter.route('/authenticate')
-	.post(usersController.authenticate)
+apiRouter.route('/auth/soundcloud')
+  .get(usersController.getSoundcloud)
+
+apiRouter.route('/auth/soundcloud/callback')
+   .get(usersController.getSoundcloudCallback)
 
 apiRouter.route('/users')
 	.get(usersController.index)
